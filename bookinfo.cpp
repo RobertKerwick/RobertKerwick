a976550602c38c79e731af75ebe3570248f4dd64
RYoung1225 <43529532+RYoung1225@users.noreply.github.com>
2019-04-12 12:43:16 -0400

    Add files via upload

---
+++ bookinfo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	cout << setw(40) << "Serendipity Booksellers" << endl;		//Header for the output
+	cout << setw(37) << "Book Information" << endl;
+	cout << endl;
+
+	//Book Info Displays - input information not requested
+	cout << "ISBN:" << endl;
+	cout << "Title:" << endl;
+	cout << "Author:" << endl;
+	cout << "Publisher:" << endl;
+	cout << "Date Added:" << endl;
+	cout << "Quantity-On-Hand:" << endl;
+	cout << "Wholesale Cost:" << endl;
+	cout << "Retail Price:" << endl;
+
+	return 0;
+}
